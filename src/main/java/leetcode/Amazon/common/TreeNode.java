2ab608fa21a1957424c2439a325de516444e47f6
bwnyasse <risbonyasse@gmail.com>
2019-01-16 23:41:44 +0100

    leetcode: BST

---
+++ src/main/java/leetcode/Amazon/common/TreeNode.java
@@ -0,0 +1,15 @@
+package leetcode.Amazon.common;
+
+/**
+ * TreeNode
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
