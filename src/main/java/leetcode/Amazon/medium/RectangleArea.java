24ca8342ff8deca1610ba427b79e4dd73bcd0088
bwnyasse <risbonyasse@gmail.com>
2019-01-18 06:31:25 +0100

    leetcode : RectangleArea

---
+++ src/main/java/leetcode/Amazon/medium/RectangleArea.java
@@ -0,0 +1,15 @@
+package leetcode.Amazon.medium;
+
+/**
+ * RectangleArea
+ */
+public class RectangleArea {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        return 0;
+    }
+}
